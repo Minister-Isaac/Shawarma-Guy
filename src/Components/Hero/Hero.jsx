470280f26cb77b23ec8ef8eba5db25bcb8a1fd70
Isaac Egbumokei <isaacubakama@gmail.com>
2024-12-30 19:03:01 +0000

    new commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState(null);
   const [showComebackMessage, setShowComebackMessage] = useState(false);
-  const [manualToggle, setManualToggle] = useState(false);
+  const [manualToggle, setManualToggle] = useState(true);
   const [isBiddingOpen, setIsBiddingOpen] = useState(false);
 
   useEffect(() => {
@@ -56,7 +56,7 @@ const Hero = () => {
 
     try {
       const response = await fetch(
-        'https://v1.nocodeapi.com/bernie85/google_sheets/YzVQmzDQuAvmwxKV?tabId=Sheet4',
+        'https://v1.nocodeapi.com/bernie85/google_sheets/TBtGfUpKxPdCRZPV?tabId=Sheet3',
         {
           method: 'POST',
           headers: {
