2dccf0a583c54d49021119a5e2419eca7f6d5435
Isaac Egbumokei <isaacubakama@gmail.com>
2024-12-31 12:57:15 +0000

    new commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
 
     try {
       const response = await fetch(
-        'https://v1.nocodeapi.com/minister18/google_sheets/BgMXeztGwOlJnvKV?tabId=Sheet6',
+        'https://v1.nocodeapi.com/minister18/google_sheets/NXICyUBOgqtCiVFw?tabId=Sheet2',
         {
           method: 'POST',
           headers: {
--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -37,7 +37,7 @@ const Results = () => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        'https://v1.nocodeapi.com/minister18/google_sheets/BgMXeztGwOlJnvKV?tabId=Sheet6',
+        'https://v1.nocodeapi.com/minister18/google_sheets/NXICyUBOgqtCiVFw?tabId=Sheet2',
         {
           method: 'GET',
           headers: {
